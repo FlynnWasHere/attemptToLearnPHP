bebb6bc6ccbc4576aeff5c40e280e876d5fe76a9
Flynn <186771298+FlynnWasHere@users.noreply.github.com>
2026-04-02 13:23:32 -0700

    Create top.php

---
+++ top.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>HTML Document Template</title>
+<!--below is bootstrap import code-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  
+  <style>
+  	body{background-image:url('https://cdn.prod.website-files.com/687e8d1b96312cc631cafec7/68c48f93cddc44bff9c7bffa_6010813f5ce191dc4701ea9b_6002086f72b72724ad01dec6_flexbox-and-css-grid.jpeg');background-repeat:repeat}
+    .container {
+    	background-color:pink;font-family: "Lucida Console", "Courier New", monospace;}
+  </style>
+
+</head>
+
+<body>
+	<div class="container">
+    	<div class="row"> <!--first row-->
+        	<p>hi</p>
+        </div> <!--first row end-->
